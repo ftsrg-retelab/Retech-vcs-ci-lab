f1b2be52dd1615a29cb3e1b22d24b9beff493b0a
Levente Bajczi <bajczilevi@gmail.com>
2020-03-02 15:57:01 +0100

    added another test

---
+++ train-system/src/test/java/hu/bme/mit/train/system/Test2.java
@@ -0,0 +1,13 @@
+package hu.bme.mit.train.system;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Test2 {
+
+	@Test
+	public void OverridingJoystickPosition_IncreasesReferenceSpeed() {
+		Assert.assertEquals(1,2);		
+	}
+}
