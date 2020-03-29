962c99ecfbf8fa793345ff942c4beee9f4b82799
Levente Bajczi <bajczilevi@gmail.com>
2020-03-29 14:21:47 +0200

    Implemented alarm state test

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,64 @@
 package hu.bme.mit.train.sensor;
 
-import org.junit.Assert;
-import org.junit.Before;
+import java.util.Arrays;
+import java.util.Collection;
+ 
 import org.junit.Test;
+import org.junit.Before;
+
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+import hu.bme.mit.train.interfaces.*;
+
+import org.junit.Assert;
+
 import static org.mockito.Mockito.*;
 
+@RunWith(Parameterized.class)
 public class TrainSensorTest {
+    
+    private final int firstSpeed;
+    private final boolean alarmAfterFirst;
+    private final int sercondSpeed;
+    private final boolean alarmAfterSecond;
+    private final TrainController trainController; 
+    private final TrainUser trainUser; 
 
-    @Before
-    public void before() {
-        // TODO Add initializations
+    public TrainSensorTest(
+        int firstSpeed,
+        boolean alarmAfterFirst,
+        int sercondSpeed,
+        boolean alarmAfterSecond) {
+            this.firstSpeed = firstSpeed;
+            this.alarmAfterFirst = alarmAfterFirst;
+            this.sercondSpeed = sercondSpeed;
+            this.alarmAfterSecond = alarmAfterSecond;
+            this.trainController = mock(TrainController.class);
+            this.trainUser = mock(TrainUser.class);
+
+    }
+
+    
+    @Parameters()
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(
+            new Object[]{  -1, true,   -1, true  },
+            new Object[]{ 501, true,  501, true  },
+            new Object[]{ 150, false,  50, true  },
+            new Object[]{ 150, false,  75, false }
+        );
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void test() {
+        TrainSensor trainSensor = new TrainSensorImpl(trainController, trainUser);
+
+        trainSensor.overrideSpeedLimit(firstSpeed);
+        if(alarmAfterFirst) verify(trainUser).setAlarmState(true);
+        else {
+            trainSensor.overrideSpeedLimit(sercondSpeed);
+            if(alarmAfterSecond) verify(trainUser).setAlarmState(true);
+        }
     }
 }
