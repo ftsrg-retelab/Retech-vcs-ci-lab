b17e517f796d43ca38944a34590289a674ba7ce6
Levente Bajczi <bajczilevi@gmail.com>
2020-03-29 12:45:49 +0200

    implemented alarm

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -7,7 +7,8 @@ public class TrainUserImpl implements TrainUser {
 
 	private TrainController controller;
 	private int joystickPosition;
-
+	private boolean alarmState = false;
+	
 	public TrainUserImpl(TrainController controller) {
 		this.controller = controller;
 	}
@@ -27,5 +28,16 @@ public class TrainUserImpl implements TrainUser {
 		this.joystickPosition = joystickPosition;
 		controller.setJoystickPosition(joystickPosition);
 	}
+
+	@Override
+    public void setAlarmState(boolean alarmState) {
+            this.alarmState = alarmState;
+    }
+	
+    @Override
+    public boolean getAlarmState() {
+            return alarmState;
+    }
+
 
 }
