ad1280a6940ba84f6a2a8b5b7b58cdcce784b255
Modko42 <beni1998@hotmail.hu>
2020-04-02 08:49:00 +0200

    - Speed now actually responds to joystick input

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -8,6 +8,19 @@ public class TrainControllerImpl implements TrainController {
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
 
+	public TrainControllerImpl(){
+		ExecutorService service = Executors.newSingleThreadExecutor();
+
+		try {
+			Runnable r = new Runnable() {
+				@Override
+				public void run() {
+					followSpeed();
+				}
+			};
+		}
+
+
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
