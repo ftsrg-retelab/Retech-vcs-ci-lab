e33c5edf5e4624c7028444160a32a626a20826b8
Modko42 <beni1998@hotmail.hu>
2020-04-02 11:06:51 +0200

    -Joystick input fixed

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,11 +2,21 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+import java.util.concurrent.*;
+
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+
+	public TrainControllerImpl(){
+		ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
+
+			Runnable r = () -> followSpeed();
+			service.scheduleAtFixedRate(r,0,100,TimeUnit.MILLISECONDS);
+	}
+
 
 	@Override
 	public void followSpeed() {
